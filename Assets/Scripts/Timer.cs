3ce30b1c5a406633f6e1b4d720ea062574686b6d
VJ <vishnu@tallyit.com.au>
2020-08-12 15:19:59 +1000

    initial setting

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public static int timerValue = 0;
+    Text timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer.text = "Timer:" +timerValue;
+    }
+}
---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    
+    public Transform moveSpot;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        waitTime = startWaitTime;
+        moveSpot.position = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      transform.position = Vector2.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+      if(Vector2.Distance(transform.position, moveSpot.position)<.2f){
+      if(waitTime <= 0){
+          moveSpot.position = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+           
+      }  else{
+          waitTime -= Time.deltaTime;
+      }
+    }}
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.tag=="spidey"){
+          moveSpot.position = new Vector2(Random.Range(minX,maxX),Random.Range(minY,maxY));
+           Timer.timerValue+=1;
+        }
+    }
+
+}
